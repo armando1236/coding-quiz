1599b32383b6999bf8a7893666eeef9f19633800
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-03 21:44:13 -0700

    add java to start button

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,52 @@
-const questionsAnswers = [
-    {
-        q:Commonly used data types DO NOT include:
-        pa1: "Number",
-        pa2: "String",
-        pa3: "Boolean",
-        pa4: "Color",
-        correct: "Color"
-    }
+var startButton = document.getElementById('start')
+var introText = document.getElementById('intro')
 
-    {
-        q: "What does Nan stand for?"
-        pa1: "no and no",
-        pa2: "Not a Number",
-        pa3: "Not an Nose",
-        correct: "Not a Number"
-    }
+startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', blankScreen)
 
-    {
-        q: "Github is a good to _____."
-        pa1: "Design websites",
-        pa2: "Work on math homework",
-        pa3: "Upload your work",
-        pa4: "chat with friends",
-        correct: "Upload your work"
-    }
-]
 
-var timerInterval = setInterval(function(){
-    timerCount --;
-    timerElement.innerHTML = timerCount;
-    if(timerCount === 0){
-      clearInterval(timerInterval);
-      loseGame();
-    }
-    if(isWin){
-      clearInterval(timerInterval);
-    };
-  },1000)
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+}
+
+function blankScreen() {
+    introText.classList.add('hide')
+}
+
+
+
+// var startQuoiz = document.getElementById("start");
+
+
+// start.addEventListener("click", function(){intro.style.display = "none" });
+
+
+// var questionsAnswers = [
+
+//         {
+//             q:"Commonly used data types DO NOT include:"
+//             pa1: "Number",
+//             pa2: "String",
+//             pa3: "Boolean",
+//             pa4: "Color",
+//             correct: "Color"
+//         }
+    
+//         {
+//             q: "What does Nan stand for?"
+//             pa1: "no and no",
+//             pa2: "Not a Number",
+//             pa3: "Not an Nose",
+//             correct: "Not a Number"
+//         }
+    
+//         {
+//             q: "Github is good for ______"
+//             pa1: "Designing websites",
+//             pa2: "Working on math homework",
+//             pa3: "Uploading your work",
+//             pa4: "chatting with friends",
+//             correct: "Upload your work"
+//         }
+//     ];
